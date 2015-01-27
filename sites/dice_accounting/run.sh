f0f2d58b83fc642676bd969feba0e4cdd543fb1b
Kevin K <kevin.kilborn@dicemobilemail.com>
2015-01-27 10:24:26 -0500

    adding redis stuff

--- sites/dice_accounting/run.sh
+++ sites/dice_accounting/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -p $2:80 $1 -v matrix.config.ini:/web/matrix/matrix/config.ini -v rest.config.ini:/web/matrix/rest/config.ini
+docker run --rm -p $2:80 $1 -v ./matrix.config.ini:/web/matrix/matrix/config.ini -v ./rest.config.ini:/web/matrix/rest/config.ini -v ./redis.ini:/etc/php.d/redis.ini
